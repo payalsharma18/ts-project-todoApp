14c9b12c39f01028bcf1c202880bdc420fa79e89
payalsharma18 <payalsharma5394@gmail.com>
2024-10-25 17:37:10 +0530

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav>
     <Link to="/" className={todosData === null ? "active" : ""} > All </Link>
-    <Link to="/?todos=active" className={todosData === "active" ? "active" : ""} > Active </Link>
+    <Link to="/?todos=active" className={todosData === "active" ? "active" : ""} > In-Progress </Link>
     <Link to="/?todos=completed" className={todosData === "completed" ? "active" :""} > Completed </Link>
    </nav>  )
 }
